100d4503b49efd654d72e3cac7f4a40fb0bb5af4
Anzo52 <azollner2014@gmail.com>
2022-04-26 12:17:22 -0600

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -2,19 +2,16 @@
 import java.net.*;
 import java.io.*;
 
-public class TCPServer
-{
+public class TCPServer {
 	//initialize socket and input stream
 	private Socket		 socket = null;
 	private ServerSocket server = null;
 	private DataInputStream in	 = null;
 
 	// constructor with port
-	public Server(int port)
-	{
+	public Server(int port) {
 		// starts server and waits for a connection
-		try
-		{
+		try {
 			server = new ServerSocket(port);
 			System.out.println("Server started");
 
@@ -30,16 +27,12 @@ public class TCPServer
 			String line = "";
 
 			// reads message from client until "Over" is sent
-			while (!line.equals("Over"))
-			{
-				try
-				{
+			while (!line.equals("Over")) {
+				try {
 					line = in.readUTF();
 					System.out.println(line);
 
-				}
-				catch(IOException i)
-				{
+				} catch(IOException i) {
 					System.out.println(i);
 				}
 			}
@@ -48,15 +41,12 @@ public class TCPServer
 			// close connection
 			socket.close();
 			in.close();
-		}
-		catch(IOException i)
-		{
+		} catch(IOException i) {
 			System.out.println(i);
 		}
 	}
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		Server server = new Server(5000);
 	}
 }
