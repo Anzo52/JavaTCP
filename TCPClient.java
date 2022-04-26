724221594bc806501e47b05710e753af31b41293
Anzo52 <azollner2014@gmail.com>
2022-04-26 12:15:43 -0600

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -2,19 +2,16 @@
 import java.net.*;
 import java.io.*;
 
-public class Client
-{
+public class TCPClient {
 	// initialize socket and input output streams
 	private Socket socket		 = null;
 	private DataInputStream input = null;
 	private DataOutputStream out	 = null;
 
 	// constructor to put ip address and port
-	public Client(String address, int port)
-	{
+	public Client(String address, int port) {
 		// establish a connection
-		try
-		{
+		try {
 			socket = new Socket(address, port);
 			System.out.println("Connected");
 
@@ -23,13 +20,9 @@ public class Client
 
 			// sends output to the socket
 			out = new DataOutputStream(socket.getOutputStream());
-		}
-		catch(UnknownHostException u)
-		{
+		} catch(UnknownHostException u) {
 			System.out.println(u);
-		}
-		catch(IOException i)
-		{
+		} catch(IOException i) {
 			System.out.println(i);
 		}
 
@@ -37,34 +30,28 @@ public class Client
 		String line = "";
 
 		// keep reading until "Over" is input
-		while (!line.equals("Over"))
-		{
-			try
-			{
+		while (!line.equals("Over")) {
+			try {
 				line = input.readLine();
 				out.writeUTF(line);
 			}
-			catch(IOException i)
-			{
+			catch(IOException i) {
 				System.out.println(i);
 			}
 		}
 
 		// close the connection
-		try
-		{
+		try {
 			input.close();
 			out.close();
 			socket.close();
 		}
-		catch(IOException i)
-		{
+		catch(IOException i) {
 			System.out.println(i);
 		}
 	}
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		Client client = new Client("127.0.0.1", 5000);
 	}
 }
